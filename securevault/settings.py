26417e65f32a022c8d25752a0519dc62b6bef3cc
Petar Valev <petrvlev51@gmail.com>
2025-08-10 18:28:45 +0300

    fix: database config

--- securevault/settings.py
+++ securevault/settings.py
@@ -77,9 +77,9 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": config("DB_NAME"),
-            "USER": config("DB_USER"),
-            "PASSWORD": config("DB_PASSWORD"),
+            "NAME": config("DB_NAME", default="postgres"),
+            "USER": config("DB_USER", default="postgres"),
+            "PASSWORD": config("DB_PASSWORD", default=""),
             "HOST": config("DB_HOST", default="localhost"),
             "PORT": config("DB_PORT", default="5432"),
         }
